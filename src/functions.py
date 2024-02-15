7271d04ba3036a5977a64bf88dc9238a3c2f4729
Sergey Korolev <bongrichard@rambler.ru>
2024-02-15 23:29:45 +0300

    изменил нейминг

--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,13 @@
 from heapq import nlargest
+
 from src.Vacancy import Vacancy
 
 
 def get_top_n(query_top_n, start_pack):
     vacancy_list = start_pack.data_json.get_data(start_pack.vacancy_path)
-    list_salary = []
+    list_vacancy = []
     for vacancy in vacancy_list:
         if vacancy['salary'] != None:
-            list_salary.append(Vacancy(vacancy))
-    res = nlargest(query_top_n, list_salary, key=lambda x: x.salary_vacancy)
+            list_vacancy.append(Vacancy(vacancy))
+    res = nlargest(query_top_n, list_vacancy, key=lambda vacancy: vacancy.salary_vacancy)
     return res
